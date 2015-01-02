78fba5a1bba791a2a08a161f74fbf70501b25e9d
ThePooN <thepoon@cartooncraft.fr>
2015-01-02 23:19:22 +0100

    Added forcespawn module
    Signed-off-by: ThePooN <thepoon@cartooncraft.fr>

---
+++ forcespawn/src/main/java/fr/cartooncraft/essentials/lag/EssentialsSpawn.java
@@ -0,0 +1,30 @@
+package fr.cartooncraft.essentials.lag;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EssentialsSpawn extends JavaPlugin implements Listener {
+	
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+		getLogger().info("CC-Essentials-ForceSpawn is loaded.");
+	}
+	
+	public void onDisable() {
+		getLogger().info("CC-Essentials-ForceSpawn is unloaded.");
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		return false;
+	}
+	
+	@EventHandler
+	public void onLogin(PlayerLoginEvent e) {
+		e.getPlayer().teleport(e.getPlayer().getWorld().getSpawnLocation());
+	}
+	
+}
